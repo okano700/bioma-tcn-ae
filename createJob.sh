a3ab46e25ba7e3a653d9ecb7c8e613949abbc13b
Emerson Yoshiaki Okano <okano700@gmail.com>
2023-07-21 14:09:03 -0300

    Create createJob.sh

---
+++ createJob.sh
@@ -0,0 +1,9 @@
+#PBS -N TCNAE_UCR
+#PBS -l select=1:ngpu=1
+#PBS -l walltime=300:00:00
+#PBS -oe
+#PBS -m abe
+#PBS -M emerson.okano@unifesp.br
+#PBS -v
+
+python ~/bioma-tcn-ae/run_UCR.py --path ~/datsets/UCR_Anomaly_FullData/\*.txt --n 249
